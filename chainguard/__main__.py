3f9f63b3f8fbd05f7fc6e5619c15e77f6e21d6b3
Vladislav Yarmak <vladislav@vm-0.com>
2019-09-14 22:07:33 +0300

    exceptions

--- chainguard/__main__.py
+++ chainguard/__main__.py
@@ -5,8 +5,29 @@ import socket
 import datetime
 from OpenSSL import SSL, crypto
 from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives import serialization
 from cryptography import x509
 from cryptography.x509.oid import ExtensionOID, NameOID
+
+
+class ChainguardException(Exception):
+    pass
+
+
+class NoCertsReceived(ChainguardException):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "No certs retrieved!"
+
+
+class NoIntermediateCertReceived(ChainguardException):
+    def __init__(self, peer_cert=None):
+        self.peer_cert = peer_cert
+
+    def __str__(self):
+        return "No intermediate cert retrieved!"
 
 
 def make_context():
@@ -31,30 +52,49 @@ def get_x509_domains(cert):
     return names
 
 
-def print_chain(context, hostname, timeout=5):
-    print('Getting certificate chain for {0}'.format(hostname))
+def scan_host(hostname, port=443, timeout=5, context=None):
+    if context is None:
+        context = SSL.Context(method=SSL.TLSv1_2_METHOD)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock = SSL.Connection(context=context, socket=sock)
     sock.set_tlsext_host_name(hostname.encode('ascii'))
     sock.settimeout(timeout)
-    sock.connect((hostname, 443))
+    sock.connect((hostname, port))
     sock.setblocking(1)
     sock.do_handshake()
-    for (idx, cert) in enumerate(sock.get_peer_cert_chain()):
-        cert = cert.to_cryptography()
-        print(' {0} s:{1}'.format(idx, cert.subject.rfc4514_string()))
-        print(' {0} i:{1}'.format(' ', cert.issuer.rfc4514_string()))
-        print(' {0} fp(SHA256)={1}'.format(' ', cert.fingerprint(SHA256()).hex()))
-        print(' {0} Names:{1}'.format(' ', get_x509_domains(cert)))
+
+    iter_certs = iter(sock.get_peer_cert_chain())
+    try:
+        peer_cert = next(iter_certs).to_cryptography()
+    except StopIteration:
+        raise NoCertsReceived()
+    try:
+        issuer_cert = next(iter_certs).to_cryptography()
+    except StopIteration:
+        raise NoIntermediateCertReceived(peer_cert.public_bytes(serialization.Encoding.PEM))
+    del iter_certs
     sock.close()
+    del sock
+
+
+    names = get_x509_domains(peer_cert)
+    issuer = issuer_cert.fingerprint(SHA256()).hex()
+    print(issuer_cert.subject)
+    return names, issuer
 
 
 def main():
     context = make_context()
     for hostname in sys.stdin:
+        hostname = hostname.strip()
         if hostname:
             hostname = hostname.strip('.').strip()
-            print_chain(context, hostname)
+            try:
+                print(scan_host(hostname, context=context))
+            except NoIntermediateCertReceived as exc:
+                print("No intermediate cert received!")
+                print("Offending certificate:")
+                print(exc.peer_cert.decode('ascii'))
 
 
 if __name__ == '__main__':  # pragma: no cover
