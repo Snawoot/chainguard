79f3a0071d8b121ef7d3b52cf68f7ce723fcd041
Vladislav Yarmak <vladislav@vm-0.com>
2019-09-14 18:52:40 +0300

    add fingerprints

--- sslscan.py
+++ sslscan.py
@@ -4,18 +4,30 @@ import sys
 import socket
 import datetime
 from OpenSSL import SSL, crypto
+import hashlib
 
+fp_hash = hashlib.sha256
 
 def make_context():
     context = SSL.Context(method=SSL.TLSv1_2_METHOD)
     return context
 
-def print_chain(context, hostname):
+def get_pubkey_fp(pubkey):
+    dump = crypto.dump_publickey(crypto.FILETYPE_ASN1, pubkey)
+    dump_digest = fp_hash(dump).hexdigest()
+    return dump_digest
+
+def get_cert_fp(cert):
+    dump = crypto.dump_certificate(crypto.FILETYPE_ASN1, cert)
+    dump_digest = fp_hash(dump).hexdigest()
+    return dump_digest
+
+def print_chain(context, hostname, timeout=5):
     print('Getting certificate chain for {0}'.format(hostname))
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock = SSL.Connection(context=context, socket=sock)
     sock.set_tlsext_host_name(hostname.encode('ascii'))
-    sock.settimeout(5)
+    sock.settimeout(timeout)
     sock.connect((hostname, 443))
     sock.setblocking(1)
     sock.do_handshake()
@@ -26,6 +38,7 @@ def print_chain(context, hostname):
     for (idx, cert) in enumerate(sock.get_peer_cert_chain()):
         print(' {0} s:{1}'.format(idx, cert.get_subject()))
         print(' {0} i:{1}'.format(' ', cert.get_issuer()))
+        print(' {0} fp(SHA256)={1}'.format(' ', get_cert_fp(cert)))
     sock.close()
 
 context = make_context()
