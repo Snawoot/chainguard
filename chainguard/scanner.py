b08932948205fb9a3e6fde7f970badec52b34241
Vladislav Yarmak <vladislav@vm-0.com>
2019-09-21 12:52:18 +0300

    tidy verifier

--- chainguard/scanner.py
+++ chainguard/scanner.py
@@ -8,26 +8,36 @@ from OpenSSL.SSL import VERIFY_PEER, VERIFY_FAIL_IF_NO_PEER_CERT
 from .utils import get_x509_domains
 
 
-def make_context(hostname):
+def make_context(verify_cb):
     context = SSL.Context(method=SSL.TLSv1_2_METHOD)
     context.set_default_verify_paths()
     context.set_verify(
         VERIFY_PEER | VERIFY_FAIL_IF_NO_PEER_CERT,
-        partial(verify_cb_prototype, hostname))
+        verify_cb)
     return context
 
 
-def verify_cb_prototype(hostname, conn, cert, errnum, depth, ok):
-    if depth == 0:
-        names = set(get_x509_domains(cert.to_cryptography()))
-        return ok and ((hostname in names) or ('.'.join(["*"] + hostname.split('.')[1:])) in names)
-    else:
-        return ok
+class ChainVerifier:
+    def __init__(self, hostname):
+        self._hostname = hostname.lower()
+
+    def verify_cb(self, conn, cert, errnum, depth, ok):
+        if depth == 0:
+            if not ok:
+                # Skip further steps if preverify failed
+                return False
+            names = set(name.rstrip('.').lower() for name in get_x509_domains(cert.to_cryptography()))
+            if self._hostname in names:
+                return True
+            wildcard = '.'.join(["*"] + self._hostname.split('.')[1:])
+            return wildcard in names
+        else:
+            return bool(ok)
 
 
-def scan_host(hostname, port=443, timeout=5, context=None):
-    if context is None:
-        context = make_context(hostname)
+def scan_host(hostname, port=443, timeout=5):
+    verifier = ChainVerifier(hostname)
+    context = make_context(verifier.verify_cb)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock = SSL.Connection(context=context, socket=sock)
     sock.set_tlsext_host_name(hostname.encode('ascii'))
